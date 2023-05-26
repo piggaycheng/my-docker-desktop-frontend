ba3969464328b1bc8aa0ce640ba2b54c157be922
piggaycheng <piggaycheng123@gmail.com>
2023-05-26 19:32:56 +0800

    add vuetify and fix vue router by change to hash mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -13,9 +17,10 @@ export default defineConfig({
   },
   build: {
     outDir: "../backend/dist",
+    emptyOutDir: true,
   },
   base: "./",
   define: {
-     __VUE_PROD_DEVTOOLS__: true
+    __VUE_PROD_DEVTOOLS__: true
   }
 })
