df81be5f4033ac6ba5d8e60b7ce421c4060bedde
piggaycheng <piggaycheng123@gmail.com>
2023-05-28 20:12:33 +0800

    add mdi icon

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,5 +1,15 @@
 // plugins/vuetify.js
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
-export default createVuetify()
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
