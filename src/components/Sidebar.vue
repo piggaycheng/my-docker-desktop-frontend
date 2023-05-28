fb22e92eb62e7a5c00134308a8e71eb92ddfb6a4
piggaycheng <piggaycheng123@gmail.com>
2023-05-28 20:13:12 +0800

    add Sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const items = ref([
+  { text: 'My Files', icon: 'mdi-folder' },
+  { text: 'Shared with me', icon: 'mdi-account-multiple' },
+  { text: 'Starred', icon: 'mdi-star' },
+  { text: 'Recent', icon: 'mdi-history' },
+  { text: 'Offline', icon: 'mdi-check-circle' },
+  { text: 'Uploads', icon: 'mdi-upload' },
+  { text: 'Backups', icon: 'mdi-cloud-upload' },
+])
+</script>
+
+<template>
+  <v-navigation-drawer permanent absolute>
+    <v-list>
+      <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.png" title="John Leider"
+        subtitle="john@google.com">
+        <template v-slot:append>
+          <v-btn size="small" variant="text" icon="mdi-menu-down"></v-btn>
+        </template>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list :lines="false" density="compact" nav>
+      <v-list-item v-for="(item, i) in items" :key="i" :value="item" active-color="primary">
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
