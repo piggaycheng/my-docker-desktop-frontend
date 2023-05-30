f514d3be27d12601e73ddf3cc975b0a60c8735be
piggaycheng <piggaycheng123@gmail.com>
2023-05-30 23:27:57 +0800

    Image page datatable

--- src/views/ImagesView.vue
+++ src/views/ImagesView.vue
@@ -1,5 +1,48 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+import { VDataTable } from 'vuetify/labs/components';
 
+const data = ref([]);
+const dataTableConfig = ref({
+  itemsPerPage: 5,
+  headers: [{
+    title: 'ID',
+    key: 'ID',
+  }, {
+    title: 'Repository',
+    key: 'Repository',
+  }, {
+    title: 'Tag',
+    key: 'Tag',
+  }, {
+    title: 'Created',
+    key: 'CreatedSince',
+  }],
+})
+
+const messageCallback = (e: MessageEvent) => {
+  if (e.data["origin-message"]) {
+    data.value = JSON.parse(e.data.content)
+  }
+}
+
+onMounted(() => {
+  window.addEventListener("message", messageCallback)
+  window.postMessage({
+    "type": "image",
+    "method": "getImages"
+  })
+})
+
+onUnmounted(() => {
+  window.removeEventListener("message", messageCallback)
+})
 </script>
 
-<template></template>
+<template>
+  <div>
+    <v-data-table :items="data" item-value="ID" :headers="dataTableConfig.headers"
+      :items-per-page="dataTableConfig.itemsPerPage">
+    </v-data-table>
+  </div>
+</template>
